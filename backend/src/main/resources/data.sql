7855b8a8a0ecb366e8557e6e847bad513dd9dde3
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-04-08 21:27:18 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,10 +13,19 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://pixabay.com/illustrations/training-course-training-3250007/','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://pixabay.com/illustrations/training-course-training-3250007','https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
 
 INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2023-07-13T20:50:07.12345Z',TIMESTAMP WITH TIME ZONE '2024-07-13T20:50:07.12345Z',1);
 INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2023-08-13T20:50:07.12345Z',TIMESTAMP WITH TIME ZONE '2024-08-13T20:50:07.12345Z',1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'https://pixabay.com/illustrations/training-course-training-3250007',1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas dúvidas',2,'https://pixabay.com/illustrations/training-course-training-3250007',2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives exclusivas para a turma',3,'https://pixabay.com/illustrations/training-course-training-3250007',0,1);
+
+
+
+
+
 
 
  
