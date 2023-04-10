92c28377b0067ea9defdf5a563933a2b5c75afb7
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-04-10 19:21:54 -0300

    Created Entity Reply and sedd in data.sql

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
@@ -3,8 +3,10 @@ package com.devsuperior.dslearnbds.entities;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +14,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -44,6 +49,20 @@ public class Topic implements Serializable {
 	@JoinColumn (name = "lesson_id")
 	private Lesson lesson;
 	
+	@ManyToOne
+	@JoinColumn (name = "reply_id")
+	private Reply answer;
+	
+	
+	@ManyToMany
+	@JoinTable(name = "tb_topic_likes",
+			   joinColumns = @JoinColumn (name = "topic_id"),
+			   inverseJoinColumns = @JoinColumn(name = "user_id")
+	)	
+	private Set<User> likes = new HashSet<>();
+	
+	@OneToMany(mappedBy = "topic")
+	private List<Reply> replies = new ArrayList<>();
 		
 	public Topic() {
 		
