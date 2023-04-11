3c4383d586f1b7e411e3dbf4b75cf1b096eaafb1
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-04-10 22:22:10 -0300

    Exceptions, validation, security

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/CourseRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/CourseRepository.java
@@ -1,5 +1,9 @@
 package com.devsuperior.dslearnbds.repositories;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long>{
 
 }
